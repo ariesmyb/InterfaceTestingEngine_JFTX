313cabedd6e0ea9ff4977888ce9123e899eca4f4
Aries <aries.myb@outlook.com>
2021-06-17 08:28:15 +0800

    Create loginVariable.py

---
+++ params/loginVariable.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+
+"""
+    @create：2020-05-14 9:50
+    @author：by aries
+    @description：
+
+"""
+
+
+class Parameters:
+    url = 'http://192.168.6.44/cas/login'
+    username = 'gmhl.lishu.chen'
+    password = '******'
+
+
+class APIParameters:
+    url = 'http://192.168.5.38:8085/agentmobile/dtb/login'
+    mobile = '18534913862'
+    password = '******'
+    phoneMAC = '874C9D3C-0563-4E24-A1E4-B57BD143EBF8'
+    data = {'mobile': mobile, 'password': password, 'phoneMAC': phoneMAC}
