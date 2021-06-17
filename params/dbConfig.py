ac9c63deca93ddb870085413eea8ecfc93a655db
Aries <aries.myb@outlook.com>
2021-06-17 08:28:08 +0800

    Create dbConfig.py

---
+++ params/dbConfig.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+
+"""
+    @create: 2020-09-04 14:34
+    @author: by aries
+    @description: None
+    
+"""
+
+oracle_dblist = {
+    'posp_agent': 'posp_agent/******@192.168.6.49:1521/orcl',
+    'posp_boss': 'posp_boss/******@192.168.6.49:1521/orcl',
+    'posp_cust': 'posp_cust/******@192.168.6.49:1521/orcl',
+    'posp_sign': 'posp_sign/******@192.168.6.49:1521/orcl',
+    'posp_trans': 'posp_trans/******@192.168.6.49:1521/orcl'
+    }
+
+mysql_dblist = {
+    "192.168.6.46": {'user': 'root', 'passwd': '******'},
+    "192.168.6.48": {'user': 'root', 'passwd': '******'},
+    "192.168.6.51": {'user': 'root', 'passwd': '******'},
+    "192.168.6.57": {'user': 'root', 'passwd': '******'}
+    }
+
+
