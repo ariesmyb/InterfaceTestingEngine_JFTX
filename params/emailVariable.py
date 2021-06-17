b2c8b1aa79038bd76c53442e028f769005492ac4
Aries <aries.myb@outlook.com>
2021-06-17 08:28:11 +0800

    Create emailVariable.py

---
+++ params/emailVariable.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+
+"""
+    @create：2020-05-14 9:50
+    @author：by aries
+    @description：
+
+"""
+
+
+class EmailParams:
+    smtp = "smtp.qq.com"
+    from_addr = "303486470@qq.com"
+    password = "******"
+    # 多个收件人,隔开
+    to_receiver = ["303486470@qq.com", ]
+    cc_receiver = ["303486470@qq.com", ]
